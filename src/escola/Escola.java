58bfd3f9b747104ff7e4d2a3fd0cb8f7611db4c8
Alti1302 <altieres.silva@hotmail.com>
2024-10-25 00:32:54 -0300

    Criado classe escola e excluido classe Exame e cliente

---
+++ src/escola/Escola.java
@@ -0,0 +1,15 @@
+package escola;
+
+public class Escola {
+	public static void main (String[] Args) {
+		
+		Aluno felipe = new Aluno();
+		
+		felipe.setNome("Felipe");
+		felipe.setIdade(12);
+		
+		System.out.println("O aluno " + felipe.getNome ()+ " tem " + felipe.getIdade() + " anos ");
+		
+	}
+
+}
